4b50381004c098f9a4b095841b5ca7ab1986dea1
Shivendu Kumar <shiv.end.you@gmail.com>
2026-01-20 16:15:35 +0530

    Add: added student's ARCHETYPEs and Programs data

---
+++ src/utils/constants.py
@@ -0,0 +1,61 @@
+# Student's ARCHETYPES
+
+ARCHETYPES = {
+    "strong_but_disengaged": {
+        "gpa": (8.2, 9.4),
+        "attendance": (25, 45),
+        "assignment_completion": (75, 95),
+        "engagement_score": (30, 45),
+        "stress_level": (4, 6),
+        "career_clarity": (5, 7)
+    },
+
+    "engaged_but_struggling": {
+        "gpa": (5.0, 7.0),
+        "attendance": (80, 95),
+        "assignment_completion": (45, 65),
+        "engagement_score": (70, 90),
+        "stress_level": (7, 9),
+        "career_clarity": (3, 5)
+    },
+
+    "high_performer_confused": {
+        "gpa": (8.0, 9.5),
+        "attendance": (80, 95),
+        "assignment_completion": (85, 95),
+        "engagement_score": (60, 75),
+        "stress_level": (6, 8),
+        "career_clarity": (3, 5)
+    },
+
+    "detached_apathetic": {
+        "gpa": (4.0, 5.5),
+        "attendance": (45, 60),
+        "assignment_completion": (35, 50),
+        "engagement_score": (30, 45),
+        "stress_level": (1, 4),
+        "career_clarity": (1, 3)
+    }
+}
+
+
+# programs type and their semesters
+
+PROGRAMS = {
+    "B.Tech": {
+        "years": 4,
+        "total_semesters": 8
+    },
+    "BCA": {
+        "years": 3,
+        "total_semesters": 6
+    },
+    "B.Sc": {
+        "years": 3,
+        "total_semesters": 6
+    },
+    "MBA": {
+        "years": 2,
+        "total_semesters": 4
+    }
+}
